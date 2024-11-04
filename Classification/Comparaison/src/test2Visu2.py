f97e17c97fd512d1aa00a270812fc86e1aaa90ae
fryousse <fryousse@IUTCLINFB1210>
2024-11-04 16:42:14 +0100

    tests supplémentaires

--- Classification/Comparaison/src/test2Visu2.py
+++ Classification/Comparaison/src/test2Visu2.py
@@ -6,19 +6,20 @@ from sklearn.neural_network import MLPClassifier
 from utils.plot_data import *
 
 # Génération d'un jeu de données de classification
-X, y = make_classification(n_samples=500, n_features=25, n_classes=2, n_informative=2, n_redundant=0, random_state=42)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X, y = make_classification(n_samples=300, n_features=15, n_classes=4, n_informative=4, n_redundant=0, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=10)
 
 plt.ion()
 
 # Création du modèle MLP
 
-mlp = MLPClassifier(hidden_layer_sizes=(8, 4), max_iter=400, random_state=42)
+mlp = MLPClassifier(hidden_layer_sizes=(32, 32), max_iter=500, random_state=42)
 
 # Afficher les données de base
 #plot_data(X_train, y_train)
 
-# Entraînement du modèle
+#fit the model for the data
 mlp.fit(X_train, y_train)
 
 # Afficher uniquement les erreurs de prédiction
@@ -27,3 +28,8 @@ plot_data(X_train, y_train, mlp)
 plot_data(X_train, y_train, mlp, show_errors=True)
 plt.ioff()
 plt.show()
+
+
+# for i in range (len(y)):
+#     print(y[i], X[i])
+    
