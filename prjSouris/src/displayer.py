d5dfb579eb394dc91da0d491346e7ad668d0844c
matheo.thierry <matheo.thierry@etu.uca.fr>
2024-12-09 09:01:14 +0100

    displayer rework again

--- prjSouris/src/displayer.py
+++ prjSouris/src/displayer.py
@@ -60,24 +60,20 @@ def DisplayData(data):
 def RawDisplayData(data):
     for cl in data:
         bodypart = {
-            "nose": {"x": [], "y": []},
-            "forepaw_R": {"x": [], "y": []},
-            "forepaw_L": {"x": [], "y": []},
-            "hindpaw_R": {"x": [], "y": []},
-            "hindpaw_L": {"x": [], "y": []},
-            "tailbase": {"x": [], "y": []}
+            "nose": [],
+            "forepaw_R": [],
+            "forepaw_L": [],
+            "hindpaw_R": [],
+            "hindpaw_L": [],
+            "tailbase": []
         }
         for i in range(1, len(cl.columns), 3):
-            part = cl.iloc[0,i]
-            if part in bodypart:
-                bodypart[part]["x"] = cl.iloc[2:, i].values
-                bodypart[part]["y"] = cl.iloc[2:, i + 1].values
+            bodypart[cl.iloc[0,i]] = ([cl.iloc[2:, i].values, cl.iloc[2:, i + 1].values])
 
         # Visualization
         fig, ax = plt.subplots(figsize=(8, 6))
-        for key, coords in bodypart.items():
-            if len(coords["x"]) != 0 and len(coords["y"]) != 0:
-                ax.scatter(coords["x"], coords["y"], label=key)
+        for key, (x, y) in bodypart.items():
+            ax.scatter(x, y, label=key)
         plt.legend()
         plt.show()
 # ================================================================
